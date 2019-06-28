c52bf2b3a7bf83623b8bda652865137de969b389
Anthony Munoz <anthony.munozm@gmail.com>
2019-06-27 23:14:52 -0500

    WI

--- models/api.py
+++ models/api.py
@@ -1,32 +1,37 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from models import ShortForm, LongFormStep1, LongFormStep2, LongFormStep3
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def short_form_post():
-    if request.method == 'POST':       
-    return listo
+    if request.method == 'POST':  
+        print("entró en el if")     
+    return "listo"
 
 
-@app.route('localhost:5000/LongFormStep1', methods=['POST'])
+@app.route('/LongFormStep1', methods=['POST', 'GET'])
 def long_form_step1_post():
-    if request.method == 'POST':       
-    return listo
+    if request.method == 'POST': 
+        print("entró en el if")     
+    return "listo"
 
 
-@app.route('localhost:5000/LongFormStep2', methods=['POST'])
+@app.route('/LongFormStep2', methods=['POST', 'GET'])
 def long_form_step2_post():
-    if request.method == 'POST':       
-    return listo
+    if request.method == 'POST':  
+        print("entró en el if")     
+    return "listo"
 
 
-@app.route('localhost:5000/LongFormStep3', methods=['POST'])
+@app.route('/LongFormStep3', methods=['POST', 'GET'])
 def long_form_step3_post():
-    if request.method == 'POST':       
-    return listo
+    if request.method == 'POST':  
+        print("entró en el if")     
+    return "listo"
         
 
 if __name__ == '__main__':
